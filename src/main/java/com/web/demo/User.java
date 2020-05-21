8b1dcbe76841529ba97a528362f63e0e9c1914b3
WJXC1791 <1607933669@qq.com>
2020-05-21 10:58:10 +0800

    web

---
+++ src/main/java/com/web/demo/User.java
@@ -0,0 +1,22 @@
+package com.web.demo;
+
+import lombok.Data;
+
+/**
+ * @CreatedBy XingChen 16079
+ * @CreatedTime 2020-05-21-0021 10:55
+ * @Description Demo
+ * @CreatedVersion 1.0
+ */
+
+@Data
+public class User {
+
+  private Integer userid;
+  private String username;
+  private Integer userohone;
+  private String useremail;
+  private String userpsw;
+  private String userpswtwo;
+
+}
